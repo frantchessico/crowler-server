e2ea0795561ef23700e15f508c4e37cb3a2a8866
Francisco Inoque <tchessico@live.com>
2020-08-27 14:59:12 -0300

    add firebase credentials

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,14 @@
 const firebase = require('firebase')
 
 const firebaseConfig = {
-    apiKey: "AIzaSyA-ZmVVLP4wVXYwLC3jaHBi6Bo_5M67rVc",
-    authDomain: "crowlerelectrical.firebaseapp.com",
-    databaseURL: "https://crowlerelectrical.firebaseio.com",
-    projectId: "crowlerelectrical",
-    storageBucket: "crowlerelectrical.appspot.com",
-    messagingSenderId: "814727810906",
-    appId: "1:814727810906:web:1b816c3861c6238ee9ed63",
-    measurementId: "G-YE1LX4DJHJ"
+  apiKey: 'AIzaSyCNUbYeGAHc264imkY1K75TsGFpurCH4j8',
+  authDomain: 'maza-gimo.firebaseapp.com',
+  databaseURL: 'https://maza-gimo.firebaseio.com',
+  projectId: 'maza-gimo',
+  storageBucket: 'maza-gimo.appspot.com',
+  messagingSenderId: '361134124536',
+  appId: '1:361134124536:web:cc73ea204b1314ddd2ba0d',
+  measurementId: 'G-98QDEFL14X'
   };
 
   firebase.initializeApp(firebaseConfig);
