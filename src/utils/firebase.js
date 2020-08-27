276967b47c632d0842e6c75ac8033dc47bd63055
Francisco Inoque <tchessico@live.com>
2020-08-27 15:08:24 -0300

    add firebase cred

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,14 +1,14 @@
 const firebase = require('firebase')
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyCNUbYeGAHc264imkY1K75TsGFpurCH4j8',
-  authDomain: 'maza-gimo.firebaseapp.com',
-  databaseURL: 'https://maza-gimo.firebaseio.com',
-  projectId: 'maza-gimo',
-  storageBucket: 'maza-gimo.appspot.com',
-  messagingSenderId: '361134124536',
-  appId: '1:361134124536:web:cc73ea204b1314ddd2ba0d',
-  measurementId: 'G-98QDEFL14X'
+    apiKey: "AIzaSyAV-an0gnYUc59_4_hFrJMLg30fHjjBtYo",
+    authDomain: "electricaguard.firebaseapp.com",
+    databaseURL: "https://electricaguard.firebaseio.com",
+    projectId: "electricaguard",
+    storageBucket: "electricaguard.appspot.com",
+    messagingSenderId: "173298613374",
+    appId: "1:173298613374:web:26c54706c5868be97e2c2f",
+    measurementId: "G-YQPNWNPKNG"
   };
 
   firebase.initializeApp(firebaseConfig);
