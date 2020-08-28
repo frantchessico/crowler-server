cde84b5a8fc56ec1eca0063450a74df7a3e7b247
Francisco Inoque <jaimeinoque20@gmail.com>
2020-08-28 03:53:02 +0200

    add socket.io

--- src/controller/getController.js
+++ src/controller/getController.js
@@ -3,18 +3,8 @@ const firebase = require('../utils/firebase')
 
 module.exports = {
     async getDataDetails(req, res) {
-        //   const result = await  DatasDetails.find();
-      const datas =  firebase.FIRESTORE.collection('results')
-     datas.get().then(snapshot => {
-         snapshot.forEach(data => {
-             const i =  data.data();
-             
-             return res.json(i)
-         })
-     }).catch(err => { 
-         return res.json(err)
-     })
-
-         
+         const result = await  DatasDetails.find();
+      
+         return res.json(result)
     }
 }
