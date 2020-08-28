552c80aedf5676d445d5200c410eab8db2f2e5d1
Francisco Inoque <jaimeinoque20@gmail.com>
2020-08-28 03:50:23 +0200

    add socket.io

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@ const cors = require('cors');
 
 const app = express();
 app.use(express.json())
+const server = require('http').Server(app);
+const io = require('socket.io')(server)
 
-app.set('port', process.env.PORT || 3333)
+
+app.use((req, res, next) => {
+    req.io = io;
+    next()
+});
+
+io.on('connection', function(socket) {
+    console.log('user connected');
+    socket.emit('crawler', 'Hey')
+})
 app.use(cors())
 
 
  app.use('/api',router);
 
-
+ app.set('port', process.env.PORT || 3333)
 
 app.listen(app.get('port'));
 console.log('Server on port', app.get('port'));
