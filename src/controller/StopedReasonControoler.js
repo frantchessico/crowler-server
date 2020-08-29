0a9fc286b4698733a4c1a9962f6f2c97e854efe2
Francisco Inoque <jaimeinoque20@gmail.com>
2020-08-29 19:40:19 +0200

    back to before configs

--- src/controller/StopedReasonControoler.js
+++ src/controller/StopedReasonControoler.js
@@ -107,44 +107,25 @@ module.exports = {
                         ch4Byh2: ch4 / h2,
                         c2h4Byc2h6: c2h4/ c2h6,
                         createdAt: Date.now(),
-                        resulOfTest: 'Without failure, keep checking'
+                        resulOfTest: 'Sem falha, continue verificando'
                      }); 
 
                     const testReuslt = await test.save();
-                  //  await firebase.FIRESTORE.collection('results').add({
-                  //    idOfDataForTest: data.id,
-                  //    c2h2Byc2h4: c2h2/c2h4,
-                  //    ch4Byh2: ch4 / h2,
-                  //    c2h4Byc2h6: c2h4/ c2h6,
-                  //    createdAt: Date.now(),
-                  //    resulOfTest: 'Without failure, keep checking'
-                  //  })
-                    
-                  // await firebase.DATABASE.ref('result'+ data.id).set({
-                  //    idOfDataForTest: data.id,
-                  //    c2h2Byc2h4: c2h2/c2h4,
-                  //    ch4Byh2: ch4 / h2,
-                  //    c2h4Byc2h6: c2h4/ c2h6,
-                  //    createdAt: Date.now(),
-                  //    resulOfTest: 'Without failure, keep checking'
-                  // })
-
-                  const ref = firebase.ADMIN.ref('result');
-                  const resultRef = ref.push({
+                   await firebase.FIRESTORE.collection('results').add({
                      idOfDataForTest: data.id,
                      c2h2Byc2h4: c2h2/c2h4,
                      ch4Byh2: ch4 / h2,
                      c2h4Byc2h6: c2h4/ c2h6,
                      createdAt: Date.now(),
-                     resulOfTest: 'Without failure, keep checking'
-                  })
+                     resulOfTest: 'Sem falha, continue verificando'
+                   })
                     return res.json(testReuslt)
                   }
                   
                  
                }
             } else {
-               return res.json({error: 'Please enter valid value'})
+               return res.json({ok: false})
             }
             
          }
--- src/controller/getController.js
+++ src/controller/getController.js
@@ -3,35 +3,17 @@ const firebase = require('../utils/firebase')
 
 
 
-// module.exports = {
-//     async getDataDetails(req, res) {
-//          const snapshot = firebase.FIRESTORE.collection('results').orderBy('createdAt', 'desc').get()
-//          snapshot.then(data => {
-//              let avas = [];
-//              data.forEach(doc => {
-//                  avas.push(doc.data())
-//              })
-//              return res.json(avas)
-//          }).catch(err => {
-//              return res.json(err)
-//          })
-//     }
-// }
-
-
 module.exports = {
     async getDataDetails(req, res) {
-         const snapshot = firebase.DATABASE.ref('result').once('value')
-         .then(data => {
-            let avas = [];
-            data.forEach(doc => {
-                console.log(doc)
-                avas.push(doc.val())
-            })
-            return res.json(avas)
+         const snapshot = firebase.FIRESTORE.collection('results').orderBy('createdAt', 'desc').get()
+         snapshot.then(data => {
+             let avas = [];
+             data.forEach(doc => {
+                 avas.push(doc.data())
+             })
+             return res.json(avas)
          }).catch(err => {
              return res.json(err)
          })
-         
     }
 }
